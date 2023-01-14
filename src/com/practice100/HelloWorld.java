caf9c4d66f37bb3642ede2583c7450f61633ede5
ankita.patel <ankita.patel@xmplify.tech>
2023-01-14 16:49:59 +0530

    "Simple java Program"

---
+++ src/com/practice100/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.practice100;
+
+public class HelloWorld {
+        // Simple java program
+        public static void main(String[] args) {
+            System.out.println("Hello World !!!!!!!!!");
+        }
+}
