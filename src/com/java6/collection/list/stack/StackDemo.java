517ff3f9566c0d1f7cd3f7a9bef736733548df65
ankita.patel <ankita.patel@xmplify.tech>
2023-02-19 14:22:00 +0530

    "Collection : Stack : Practice"

--- src/com/java6/collection/list/stack/StackDemo.java
+++ src/com/java6/collection/list/stack/StackDemo.java
@@ -5,17 +5,17 @@ import java.util.Stack;
 public class StackDemo {
     public static void main(String[] args) {
         Stack stack = new Stack();
-        System.out.println("Stack initial capacity : "+stack.capacity());
+        System.out.println("Stack initial capacity : " + stack.capacity());
 
         stack.push(1);
         stack.push("A");
         stack.push(25);
-        System.out.println("Stack peek operation result : "+stack.peek());
+        System.out.println("Stack peek operation result : " + stack.peek());
         stack.push(45);
-        System.out.println("Stack push operation result : "+stack);
-        System.out.println("Stack search() operation result : "+stack.search("A"));
-        System.out.println("Stack search() operation result2 : "+stack.search(5));
-        System.out.println("Stack peek operation result : "+stack.pop());
+        System.out.println("Stack push operation result : " + stack);
+        System.out.println("Stack search() operation result : " + stack.search("A"));
+        System.out.println("Stack search() operation result2 : " + stack.search(5));
+        System.out.println("Stack peek operation result : " + stack.pop());
 
     }
 }
