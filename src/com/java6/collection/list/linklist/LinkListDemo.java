92ec0c8f3f25187cae6ba7fc98d8990073387649
ankita.patel <ankita.patel@xmplify.tech>
2023-02-19 14:23:14 +0530

    "Collection : Formatting"

--- src/com/java6/collection/list/linklist/LinkListDemo.java
+++ src/com/java6/collection/list/linklist/LinkListDemo.java
@@ -4,23 +4,23 @@ import java.util.LinkedList;
 
 public class LinkListDemo {
     public static void main(String[] args) {
-        System.out.println("Linked List Demo : ");
 
+        System.out.println("Linked List Demo : ");
         LinkedList list = new LinkedList();
+
         list.add("abc");
         list.add(102);
-        System.out.println("Linked List 1 "+list);
+        System.out.println("Linked List 1 " + list);
         list.addFirst("105");
-        System.out.println("Linked List 2 "+list);
+        System.out.println("Linked List 2 " + list);
         list.addLast("hii");
-        list.set(1,"xyz");
-        System.out.println("Linked List 3 "+list);
-        System.out.println("Get Last : "+list.getLast());
+        list.set(1, "xyz");
+        System.out.println("Linked List 3 " + list);
+        System.out.println("Get Last : " + list.getLast());
         list.removeLast();
         list.add("python");
-        list.add(3,"java");
-        System.out.println("Linked List 4 "+list);
-
+        list.add(3, "java");
+        System.out.println("Linked List 4 " + list);
 
 
     }
--- src/com/java6/collection/list/vector/VectorDemo.java
+++ src/com/java6/collection/list/vector/VectorDemo.java
@@ -4,18 +4,18 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector vector = new Vector();
-        System.out.println("Initial capacity of vector : "+vector.capacity());
 
-        for(int i = 1 ; i<=10; i++){
+        Vector vector = new Vector();
+        System.out.println("Initial capacity of vector : " + vector.capacity());
+
+        for (int i = 1; i <= 10; i++) {
             vector.addElement(i);
         }
         vector.addElement("A");
-        System.out.println("Capacity Increase : "+vector.capacity());
+        System.out.println("Capacity Increase : " + vector.capacity());
         System.out.println(vector);
         vector.removeElement("A");
         vector.removeElement(2);
         System.out.println(vector);
-
     }
 }
