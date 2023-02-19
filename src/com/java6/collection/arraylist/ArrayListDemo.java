07ebe7e725689d47913051baf315409b2fec4a30
ankita.patel <ankita.patel@xmplify.tech>
2023-02-19 11:59:42 +0530

    "Collection : ArrayList : Practice"

---
+++ src/com/java6/collection/arraylist/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.java6.collection.arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add("A");
+        al.add("abc");
+        al.add(10);
+        al.add(null);
+        System.out.println("Print the added object in ArrayList : "+al);
+        al.remove(2);
+        System.out.println("After removing object : "+al);
+        al.add(2,"M");
+        al.add(20);
+        System.out.println("ArrayList : "+al);
+    }
+
+}
